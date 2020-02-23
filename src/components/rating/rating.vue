e47e1c83f6fcbdc325145abb2ca3030a75280c85
panshicheng <x_pan2016@163.com>
2020-02-23 18:32:13 +0800

    安装cube-ui；shocart组件 computed动态监听数据变化，保证了页面实时响应；vue.set()

--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -4,127 +4,313 @@
 * Description:
 */
 <template>
-  <div class="alarm-list">
-    <div class="select-group"/>
-    <div class="list-scroll-wrap">
-      <cube-scroll
-        ref="alarmlistscroll"
-        :data="items"
-        :options="options"
-        :scroll-events="['scroll']"
-        @scroll="onScrollHandle"
-        @pulling-down="onPullingDown"
-        @pulling-up="onPullingUp">
-        <ul>
-          <li v-for="(item,index) in items" :key="index" class="item">
-            {{ item }}
-          </li>
-        </ul>
-        <!--  下拉刷新  需要加上:cube-pulldown-wrapper  定位下拉盒子内容在content顶部 :style="pullDownStyle" -->
-        <template slot="pulldown" slot-scope="props">
-          <div v-if="props.pullDownRefresh" :style="pullDownStyle" class="cube-pulldown-wrapper">
-            <!--  自定义下拉刷新的内容   -->
-            <div class="pulldown-content">
-              <span v-if="props.beforePullDown">下拉刷新...</span>
-              <div v-else>
-                <span v-if="props.isPullingDown">正在更新...</span>
-                <span v-else>更新成功</span>
-              </div>
+  <div ref="ratingspage" class="ratings">
+    <div>
+      <div class="ratings-content">
+        <div class="overview">
+          <div class="overview-left">
+            <h1 class="score">{{ seller.score }}</h1>
+            <div class="title">综合评分</div>
+            <div class="rank">高于周边商家{{ seller.rankRate }}%</div>
+          </div>
+          <div class="overview-right">
+            <div class="score-wrapper">
+              <span class="title">服务态度</span>
+              <star :size="36" :score="seller.serviceScore"/>
+              <span class="score">{{ seller.serviceScore }}</span>
+            </div>
+            <div class="score-wrapper">
+              <span class="title">商品评分</span>
+              <star :size="36" :score="seller.foodScore"/>
+              <span class="score">{{ seller.foodScore }}</span>
+            </div>
+            <div class="delivery-wrapper">
+              <span class="title">送达时间</span>
+              <span class="delivery">{{ seller.deliveryTime }}分钟</span>
             </div>
           </div>
-        </template>
-        <template slot="pullup" slot-scope="props">
-          <div v-if="props.pullUpLoad" class="cube-pullup-wrapper" style="height: 0.8rem">
-            <p v-if="props.isPullUpLoad">  加载中.....</p>
-          </div>
-        </template>
-      </cube-scroll>
+        </div>
+      </div>
+      <split />
+      <ratingselect
+        :ratings="ratings"
+        :only-content="onlyContent"
+        :select-type="selectType"
+        @content="toggleContent"
+        @select="toggleSelect"/>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="(rating,index) in ratings" v-show="needShow(rating.rateType, rating.text)" :key="index" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar" width="28" height="28">
+            </div>
+            <div class="content">
+              <h1 class="name">{{ rating.username }}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"/>
+                <span v-show="rating.deliveryTime" class="delivery">{{ rating.deliveryTime }}</span>
+              </div>
+              <p class="text">{{ rating.text }}</p>
+              <div v-show="rating.recommend && rating.recommend.length" class="recommend">
+                <span class="icon-thumb_up"/>
+                <span v-for="(item,index) in rating.recommend" :key="index" class="item">{{ item }}</span>
+              </div>
+              <div class="time">
+                {{ rating.rateTime | formatDate }}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+import Star from '../../base/star/star'
+import Split from '../split/split'
+import Ratingselect from '../ratingselect/ratingselect'
+import { ERR_OK } from '../../api/const'
+import { formatDate } from '../../common/js/date'
+
+const ALL = 2
 
 export default {
-  data() {
-    return {
-      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      /* options 参数配置参考better-scroll 的官网*/
-      options: {
-        pullDownRefresh: {
-          threshold: 40,
-          stop: 50
-        },
-        pullUpLoad: {
-          threshold: 100
-        }
-      },
-      pullDownY: 0,
-      pullDownStyle: ''
+  components: {
+    Ratingselect,
+    Split,
+    Star
+  },
+  filters: {
+    formatDate(time) {
+      const date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh:mm')
     }
   },
-  computed: {
-    /* 自定义下拉到一定高度你想展示的内容 */
-    // pullDownTip() {
-    //     if (this.pullDownY <= 60) {
-    //         return '下拉刷新...'
-    //     } else if (this.pullDownY <= 90) {
-    //         return '继续下拉有惊喜...'
-    //     } else {
-    //         return '松手得惊喜！'
-    //     }
-    // },
+  props: {
+    seller: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      ratings: [],
+      selectType: ALL,
+      onlyContent: true
+    }
+  },
+  created() {
+    this._getRatings()
   },
   methods: {
-    /* 根据滚动事件 计算下拉刷新的位置，当自定的下拉刷新动画盒子定位在盒子顶部 */
-    onScrollHandle(pos) {
-      // console.log(pos)
-      this.pullDownY = pos.y
-      if (pos.y > 0) {
-        this.pullDownStyle = `top:${pos.y - 30}px`
+    needShow(type, text) {
+      if (this.onlyContent && !text) {
+        return false
+      }
+      if (this.selectType === ALL) {
+        return true
+      } else {
+        return this.selectType === type
       }
     },
-    /*  下拉刷新完成 ： this.$refs['alarm-list-scroll'].forceUpdate() 强制执行下拉盒子的高度 */
-    onPullingDown() {
-      setTimeout(() => {
-        this.$refs['alarmlistscroll'].forceUpdate()
-      }, 1000)
+    toggleContent() {
+      this.onlyContent = !this.onlyContent
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
     },
-    /* 上拉加载  */
-    onPullingUp() {
-      setTimeout(() => {
-        for (let i = 0; i < 3; i++) {
-          this.items.push(i + '=')
+    toggleSelect(type) {
+      this.selectType = type
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    },
+    _getRatings() {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body
+        if (response.code === ERR_OK) {
+          this.ratings = response.data
+          this.$nextTick(() => {
+            this._initScroll()
+          })
         }
-        this.$refs['alarmlistscroll'].forceUpdate()
-      }, 3000)
+      })
+    },
+    _initScroll() {
+      if (!this.scroll) {
+        this.scroll = new BScroll(this.$refs.ratingspage, {
+          click: true
+        })
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .alarm-list {
-    height: 100vh;
-  }
-
-  .select-group {
-    height: 1.5rem;
-  }
-
-  .list-scroll-wrap {
-    height: calc(100vh - 1.5rem);
-    background: lavender;
-
-    .item {
-      height: 1.8rem;
-      background: lightblue;
-      margin: 0.3rem;
-
-      &:nth-child(1) {
-        margin-top: 0;
+@import "../../common/stylus/mixin.styl"
+.ratings{
+  position: absolute
+  top: 174px
+  width: 100%
+  bottom: 0
+  left: 0
+  overflow: hidden
+  .ratings-content{
+    .overview{
+      display: flex
+      padding: 18px 0
+      .overview-left{
+        flex: 0 0 137px //flex布局,固定左边,右边自动适配
+        padding: 6px 0
+        width: 137px
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        @media only screen and (max-width: 320px) { //适配i5屏幕
+          flex: 0 0 120px
+          width: 120px
+        }
+        .score{
+          margin-bottom: 6px
+          font-size: 24px
+          line-height: 28px
+          color: rgb(255, 153, 0)
+        }
+        .title{
+          margin-bottom: 8px
+          font-size: 12px
+          line-height: 12px
+          color: rgb(7, 17, 27)
+        }
+        .rank{
+          margin-bottom: 6px
+          font-size: 10px
+          line-height: 10px
+          color: rgb(147, 153, 159)
+        }
+      }
+      .overview-right{
+        flex: 1
+        padding: 6px 0 6px 24px
+        @media only screen and (max-width: 320px) {
+          padding-left: 6px
+        }
+        .score-wrapper{
+          margin-bottom: 8px
+          font-size: 0
+          .title{
+            display: inline-block
+            vertical-align: top
+            font-size: 12px
+            line-height: 18px
+            color: rgb(7, 17, 27)
+          }
+          .star{
+            display: inline-block
+            vertical-align: top
+            padding: 0 12px
+          }
+          .score{
+            display: inline-block
+            vertical-align: top
+            font-size: 12px
+            line-height: 18px
+            color: rgb(255, 153, 0)
+          }
+        }
+        .delivery-wrapper{
+          .title, .delivery{
+            display: inline-block
+            font-size: 12px
+            line-height: 18px
+          }
+          .title{
+            color: rgb(7, 17, 27)
+          }
+          .delivery{
+            padding-left: 12px
+            color: rgb(147, 153, 159)
+          }
+        }
       }
     }
   }
-
+  .rating-wrapper {
+    padding: 0 18px
+    .rating-item {
+      display: flex
+      padding: 18px 0
+      border-1px(rgba(7, 17, 27, 0.1))
+      .avatar {
+        flex: 0 0 28px
+        width: 28px
+        margin-right: 12px
+        img {
+          border-radius: 50%
+        }
+      }
+      .content {
+        position: relative
+        flex: 1
+        .name {
+          margin-bottom: 4px
+          line-height: 12px
+          font-size: 10px
+          color: rgb(7, 17, 27)
+        }
+        .star-wrapper {
+          margin-bottom: 6px
+          font-size: 0
+          .star {
+            display: inline-block
+            margin-right: 6px
+            vertical-align: top
+          }
+          .delivery {
+            display: inline-block
+            vertical-align: top
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          }
+        }
+        .text {
+          margin-bottom: 8px
+          line-height: 18px
+          color: rgb(7, 17, 27)
+          font-size: 12px
+        }
+        .recommend {
+          line-height: 16px
+          font-size: 0
+          .icon-thumb_up, .item {
+            display: inline-block
+            margin: 0 8px 4px 0
+            font-size: 9px
+          }
+          .icon-thumb_up {
+            color: rgb(0, 160, 220)
+          }
+          .item {
+            padding: 0 6px
+            border: 1px solid rgba(7, 17, 27, 0.1)
+            border-radius: 1px
+            color: rgb(147, 153, 159)
+            background: #fff
+          }
+        }
+        .time {
+          position: absolute
+          top: 0
+          right: 0
+          line-height: 12px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        }
+      }
+    }
+  }
+}
 </style>
